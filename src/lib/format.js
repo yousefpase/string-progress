e5bda0d123f0a1b7e4c9de55bebb77aa5e3c5d92
yousefpase <76731007+yousefpase@users.noreply.github.com>
2021-01-08 11:50:21 +0200

    Update format.js

--- src/lib/format.js
+++ src/lib/format.js
@@ -1,1 +1,23 @@
 
+const normal_slider = "🔘";
+const normal_line = "▬";
+const normal_size = 20;
+
+const calculator = require("./calculat.js");
+
+const bar = function(total, current, options) {
+  options = options || {};
+  var slider, line, size;
+  if (options.slider) slider = options.slider;
+  if (!options.slider) slider = normal_slider;
+  
+  if (options.line) line = options.line;
+  if (!options.line) line = normal_line;
+  
+  if (options.size) size = options.size;
+  if (!options.size) size = normal_size;
+
+  return calculator(total, current, size, slider, line);
+};
+
+module.exports = bar;
