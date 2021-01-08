96892e2f665ecae2fc04634e00da4ac2096cb916
yousefpase <76731007+yousefpase@users.noreply.github.com>
2021-01-08 11:51:19 +0200

    Update calculat.js

--- src/lib/calculat.js
+++ src/lib/calculat.js
@@ -1,1 +1,15 @@
 
+const options = require('./format.js');
+
+function calculat(total, current, size, slider, line) {
+  if (current < 3) current = 3;
+  var division = current / total;
+  var bar = Math['round']((size * division));
+  var noline = size - bar;
+  var bar_complate = line.repeat(bar).replace(/.$/, slider);
+  var empty = line.repeat(noline);
+  bar = bar_complate + empty;
+  
+  return bar;
+}
+module.exports = calculat;
