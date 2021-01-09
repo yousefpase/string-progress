6e5c32a48a69610e0055f06c229c7f11f9a7e358
yousefpase <76731007+yousefpase@users.noreply.github.com>
2021-01-09 13:43:13 +0200

    Update format.js

--- src/lib/format.js
+++ src/lib/format.js
@@ -1,4 +1,3 @@
-
 const normal_slider = "🔘";
 const normal_line = "▬";
 const normal_size = 20;
@@ -20,4 +19,35 @@ const bar = function(total, current, options) {
   return calculator(total, current, size, slider, line);
 };
 
-module.exports = bar;
+const __format = function(sec, options) {
+  options = options || {};
+  if ('object' !== typeof options || !(options instanceof Object)) {
+    throw new Error('Options must be Object');
+    return null;
+  }
+  sec = sec || 0;
+  if (sec && sec == '' || isNaN(sec)) {
+    throw new Error('Seconds must be Number');
+    return null;
+  }
+  sec = sec ? Math.floor(sec) : 0;
+  
+  if (sec < 1) return '00:00';
+  
+  var date = new Date(sec * 1000);
+  
+  date = date.toISOString();
+  
+  if (sec == 3600 || sec > 3600) {
+    date = date.substr(11,8);
+  } else {
+    date = date.substr(14,5);
+  }
+  
+  return date;
+}
+
+module.exports = {
+  bar,
+  __format
+};
